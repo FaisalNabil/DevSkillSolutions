6f5b99b3083765d3129cad74d96da80f657ca07f
Faisal Nabil <nabilt59@gmail.com>
2016-11-06 12:24:38 +0600

    Dev skill problem solutions

--- Circle and Square.java
+++ Circle and Square.java
@@ -3,6 +3,7 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
+import java.math.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone
@@ -10,5 +11,15 @@ class Ideone
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner in=new Scanner(System.in);
+		int testCase=in.nextInt();
+		for(int i=0;i<testCase;i++){
+			double r=in.nextDouble();
+			double a=in.nextDouble();
+			if( (int)(r*2) == (int)(Math.sqrt(2)*a) )
+				System.out.println("Yes");
+			else
+				System.out.println("No");
+		}
 	}
 }
--- Reverse.java
+++ Reverse.java
@@ -11,6 +11,16 @@ class Ideone
 	{
 		// your code goes here
 		Scanner in=new Scanner(System.in);
-		int testCase
+		int testCase=in.nextInt();
+		for(int x=0;x<testCase;x++){
+			String s=in.nextLine();
+			int length=s.length()-1;
+			for(int i=length;i>=0;i--){
+				if(Character.isLetter(s.charAt(i)) || s.charAt(i)==' ' || s.charAt(i)=='.')
+					System.out.print(s.charAt(i));
+			}
+			
+			System.out.println();
+		}
 	}
 }
